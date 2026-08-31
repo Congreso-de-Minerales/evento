4241ee707499c484cc1ce6df15b20bc7415a388d
Ing.Isaias Briano <darkneoevo@hotmail.com>
2026-08-31 13:02:28 -0600

    Automated deployment to GitHub Pages

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -1,6 +1,5 @@
-<footer class="w-full bg-[#111] border-t border-white/10 mt-auto flex justify-center py-6 px-4 lg:px-8">
-    <div class="w-full max-w-7xl">
+<footer class="w-full bg-black border-t border-white/10 mt-auto flex justify-center py-8 px-4 lg:px-8">
+    <div class="w-full max-w-7xl relative group overflow-hidden rounded-2xl shadow-2xl shadow-orange-500/10 border border-white/5 bg-white/5 transition-all duration-500 hover:-translate-y-1 hover:shadow-orange-500/20">
         <img src="{{ asset('assets/footer.jpeg') }}" alt="Logos y Patrocinadores" class="w-full h-auto object-contain">
     </div>
 </footer>
-
